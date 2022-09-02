cb1ceb206e677d9fc0eb9f4e707ac5b7483041b9
evelo <evelo0702@gmail.com>
2022-09-02 12:06:12 +0900

    api 수정

--- 7.array-api원본.js
+++ 7.array-api원본.js
@@ -1,21 +1,28 @@
 // Q1. make a string out of an array
 {
-  const fruits = ['apple', 'banana', 'orange'];
+  const fruits = ["apple", "banana", "orange"];
+  const strFruits = fruits.join();
+  console.log(strFruits);
+  console.log(typeof strFruits);
 }
-
 // Q2. make an array out of a string
 {
-  const fruits = '🍎, 🥝, 🍌, 🍒';
+  const fruits = "🍎, 🥝, 🍌, 🍒";
+  const arrFruits = fruits.split(",");
+  console.log(arrFruits);
 }
 
 // Q3. make this array look like this: [5, 4, 3, 2, 1]
 {
   const array = [1, 2, 3, 4, 5];
+  console.log(array.reverse());
 }
 
 // Q4. make new array without the first two elements
 {
   const array = [1, 2, 3, 4, 5];
+  const array2 = array.slice(2);
+  console.log(array2);
 }
 
 class Student {
@@ -27,40 +34,83 @@ class Student {
   }
 }
 const students = [
-  new Student('A', 29, true, 45),
-  new Student('B', 28, false, 80),
-  new Student('C', 30, true, 90),
-  new Student('D', 40, false, 66),
-  new Student('E', 18, true, 88),
+  new Student("A", 29, true, 45),
+  new Student("B", 28, false, 80),
+  new Student("C", 30, true, 90),
+  new Student("D", 40, false, 66),
+  new Student("E", 18, true, 88),
 ];
+console.log(students);
 
 // Q5. find a student with the score 90
 {
+  for (score of students) {
+    if (score.score == 90) {
+      console.log(score);
+    }
+  }
 }
 
 // Q6. make an array of enrolled students
 {
+  const enrolledStudent = [];
+  for (student of students) {
+    if (student.enrolled == true) {
+      enrolledStudent.push(student);
+    }
+  }
+  console.log(enrolledStudent);
 }
 
 // Q7. make an array containing only the students' scores
 // result should be: [45, 80, 90, 66, 88]
 {
+  const score = [];
+  for (student of students) {
+    score.push(student.score);
+  }
+  console.log(score);
 }
 
 // Q8. check if there is a student with the score lower than 50
 {
+  for (student of students) {
+    if (student.score < 50) {
+      console.log(student.name);
+    }
+  }
 }
 
 // Q9. compute students' average score
 {
+  let avgscore = 0;
+  for (student of students) {
+    avgscore += student.score;
+  }
+  avgscore = avgscore / students.length;
+  console.log(avgscore);
 }
 
 // Q10. make a string containing all the scores
 // result should be: '45, 80, 90, 66, 88'
 {
+  const score = [];
+
+  for (student of students) {
+    score.push(student.score);
+  }
+  const strscore = score.join();
+  console.log(strscore);
 }
 
 // Bonus! do Q10 sorted in ascending order
 // result should be: '45, 66, 80, 88, 90'
 {
+  const score = [];
+
+  for (student of students) {
+    score.push(student.score);
+  }
+  const strscore = score.sort().join();
+  console.log(strscore);
 }
